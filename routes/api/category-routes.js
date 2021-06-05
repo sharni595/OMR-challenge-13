aeab8d66e78243d81922d964097cfbbc7142ec1f
Sharni Zaugg <sharni.greener@gmail.com>
2021-06-05 17:58:01 -0600

    complete tag methods

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -53,7 +53,13 @@ router.post('/', (req, res) => {
   Category.create({
     category_name: req.body.category_name
   })
-    .then(dbCategoryData => res.json(dbCategoryData))
+    .then(dbCategoryData => {
+      if(!dbCategoryData){
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+      }
+      res.json(dbCategoryData)
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
