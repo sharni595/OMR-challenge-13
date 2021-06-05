788caf44efaabf78c037f98adde0d59ae64a331d
Sharni Zaugg <sharni.greener@gmail.com>
2021-06-05 17:32:50 -0600

    complete product methods

--- models/index.js
+++ models/index.js
@@ -17,14 +17,14 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'tagged-products',
+  as: 'tags',
   foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'tagged-products',
+  as: 'tags',
   foreignKey: 'tag_id'
 })
 
