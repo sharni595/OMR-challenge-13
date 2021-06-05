3748aaf119087c776c3e5e33a3126345dd0eda3b
Sharni Zaugg <sharni.greener@gmail.com>
2021-06-05 15:37:52 -0600

    create all category methods

--- models/Product.js
+++ models/Product.js
@@ -33,6 +33,13 @@ Product.init(
       validate: {
         isNumeric: true
       }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
     }
   },
   {
--- models/index.js
+++ models/index.js
@@ -6,12 +6,12 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category,{
-  foreign_key: 'category_name'
+  foreign_key: 'category_id'
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_name'
+  foreignKey: 'category_id'
 });
 
 // Products belongToMany Tags (through ProductTag)
